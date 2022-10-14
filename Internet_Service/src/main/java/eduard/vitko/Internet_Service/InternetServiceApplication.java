662bee344f96d904c6ab5c8780197c13b86c3a3a
eduar <eduardvitko>
2022-10-14 21:41:42 +0300

    Initial commit

---
+++ Internet_Service/src/main/java/eduard/vitko/Internet_Service/InternetServiceApplication.java
@@ -0,0 +1,13 @@
+package eduard.vitko.Internet_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetServiceApplication.class, args);
+	}
+
+}
---
+++ Internet_Service/src/test/java/eduard/vitko/Internet_Service/InternetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eduard.vitko.Internet_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
