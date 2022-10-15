a357479578501bee4a79830671683e2e19e12fe5
eduar <eduardvitko>
2022-10-15 11:47:12 +0300

    create UserResource and SecurityConfig and changes application.properties and setting create users in InternetServiceApplication

--- Internet_Service/src/main/java/eduard/vitko/Internet_Service/InternetServiceApplication.java
+++ Internet_Service/src/main/java/eduard/vitko/Internet_Service/InternetServiceApplication.java
@@ -1,7 +1,16 @@
 package eduard.vitko.Internet_Service;
 
+import eduard.vitko.Internet_Service.domain.Role;
+import eduard.vitko.Internet_Service.domain.User;
+import eduard.vitko.Internet_Service.services.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class InternetServiceApplication {
@@ -9,5 +18,11 @@ public class InternetServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InternetServiceApplication.class, args);
 	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 
 }
